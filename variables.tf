cda97c5748e6ab4e9170e8e63e273769f0a4d9b3
aazdiallo <a.azdiallo@gmail.com>
2021-12-04 18:37:51 -0600

    adding var files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+    default = "AKIA3VZ5IJCP4ZL36LBP"
+}
+
+variable "secret_key" {
+    default = "u8HJ1b4HlSL5RCftEQpD3TqCwL5duo5MsP56syeq"
+}
+
+variable "region" {
+    default = "us-east-1"
+}
