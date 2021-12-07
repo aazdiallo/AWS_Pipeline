792002a740121d995f1c698b6f04d7829d0f8221
aazdiallo <a.azdiallo@gmail.com>
2021-12-07 07:58:19 -0600

    default

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "app_server" {  
+resource "aws_instance" "default" {  
   ami                             = "ami-0ed9277fb7eb570c9"  
   instance_type                   = "t2.nano"
   associate_public_ip_address     = false
