8f51f18554b273eba9e752d235e6b75de592ba68
aazdiallo <a.azdiallo@gmail.com>
2021-12-08 15:24:37 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,28 +1,69 @@
-# Create a VPC
-resource "aws_vpc" "ec2_training" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+# Create VPC
+# terraform aws create vpc
+resource "aws_vpc" "vpc_training" {
+  cidr_block              = "${var.vpc-cidr}"
+  instance_tenancy        = "default"
+  enable_dns_hostnames    = true
 
-  tags = {
-    Name = "Training VPC"
+  tags      = {
+    Name    = "Training VPC"
   }
 }
 
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.ec2_training.id
-  cidr_block = "10.0.1.0/24"
+# Create Internet Gateway and Attach it to VPC
+# terraform aws create internet gateway
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id    = aws_vpc.vpc_training.id
 
-  tags = {
-    Name = "Private Subnet"
+  tags      = {
+    Name    = "Traing IGW"
   }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.ec2_training.id
-  cidr_block = "10.0.0.0/24"
+# Create Public Subnet
+# terraform aws create subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id                  = aws_vpc.vpc_training.id
+  cidr_block              = "${var.public-subnet-cidr}"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
-  tags = {
-    Name = "Public Subnet"
+  tags      = {
+    Name    = "Public Subnet"
   }
 }
 
+# Create Route Table and Add Public Route
+# terraform aws create route table
+resource "aws_route_table" "public-route-table" {
+  vpc_id       = aws_vpc.vpc_training.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
+
+  tags       = {
+    Name     = "Public Route Table"
+  }
+}
+
+# Associate Public Subnet to "Public Route Table"
+# terraform aws associate subnet with route table
+resource "aws_route_table_association" "public-subnet-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+
+# Create Private Subnet
+# terraform aws create subnet
+resource "aws_subnet" "private-subnet" {
+  vpc_id                   = aws_vpc.vpc_training.id
+  cidr_block               = "${var.private-subnet-cidr}"
+  availability_zone        = "us-east-1a"
+  map_public_ip_on_launch  = false
+
+  tags      = {
+    Name    = "Private Subnet"
+  }
+}
