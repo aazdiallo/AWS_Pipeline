3595f0e00c38ff9b81325a2a44a1291fdcba3d71
aazdiallo <a.azdiallo@gmail.com>
2021-12-08 15:30:28 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,4 +8,23 @@ variable "secret_key" {
 
 variable "region" {
     default = "us-east-1"
+    type    = string
+}
+
+variable "vpc-cidr" {
+  default       = "10.0.0.0/16"
+  description   = "VPC CIDR Block"
+  type          = string
+}
+
+variable "public-subnet-cidr" {
+  default       = "10.0.0.0/24"
+  description   = "Public Subnet CIDR Block"
+  type          = string
+}
+
+variable "private-subnet-cidr" {
+  default       = "10.0.2.0/24"
+  description   = "Private Subnet CIDR Block"
+  type          = string
 }
