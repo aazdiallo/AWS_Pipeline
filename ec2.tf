1ebba84711075e90b36718adbea589a2ea407e42
aazdiallo <a.azdiallo@gmail.com>
2021-12-08 15:09:27 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "default" {  
+resource "aws_instance" "ec2-training" {  
   ami                             = "ami-0ed9277fb7eb570c9"  
   instance_type                   = "t2.nano"
   associate_public_ip_address     = false
