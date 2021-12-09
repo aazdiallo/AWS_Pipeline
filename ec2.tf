96016c16cb49401b9ac32c7cdffc415e478cede0
aazdiallo <a.azdiallo@gmail.com>
2021-12-09 09:32:39 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ec2_training" {
   ami                             = "ami-0ed9277fb7eb570c9"  
   instance_type                   = "t2.nano"
   associate_public_ip_address     = false
+  subnet_id                       = "subnet-0800c6e2509593ba0"
   
   tags = {    
     Name    = "ExampleEC2"
